22b86484a6f15922ce59fd51d3fea6c2a73742af
Hosein Nikkhah <116885462+hoseinnikkhah@users.noreply.github.com>
2023-02-21 21:52:29 +0330

    Add files via upload

---
+++ Gcore-IP-range-finder.py
@@ -0,0 +1,20 @@
+import json
+from urllib.request import urlopen
+url = "https://api.gcorelabs.com/cdn/public-ip-list"
+response = urlopen(url)
+data = json.loads(response.read())
+array = []
+array6 = []
+for i in data['addresses']:
+    array.append(i)
+f_path = (r"ip.txt")
+with open (f_path ,'w') as d:
+       for lang in array:
+        d.write("{}\n".format(lang))
+for j in data['addresses_v6']:
+    array6.append(j)
+v_path = (r"ipv6.txt")    
+with open (v_path ,'w') as c:
+       for lang in array6:
+        c.write("{}\n".format(lang))    
+response.close()
